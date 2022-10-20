b5759249935cca3810db8bd94e799c588026460a
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-20 21:06:03 +0000

    Refactor getUserWithEmail to refer to psql database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,17 +18,20 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+  return pool
+  .query(`
+    SELECT * 
+    FROM users
+    WHERE LOWER(email) = $1;`,
+    [email.toLowerCase()])
+  .then((user) => {
+    return user.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+};
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
